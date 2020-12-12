d2238358154f544273e8dde9acdd07c4c7b054df
Dan Johnson <danjohnsonnj@gmail.com>
2020-12-12 11:35:20 -0500

    Add option to not use native scroll

--- example/scrollBy.js
+++ example/scrollBy.js
@@ -2,7 +2,7 @@ const { animateScrollBy } = require('../index.js');
 
 document.querySelectorAll('section').forEach((section) => {
   section.addEventListener('click', () => {
-    // animateScrollBy(0, window.innerHeight);
-    animateScrollBy({ x: 0, y: window.innerHeight });
+    animateScrollBy(0, window.innerHeight);
+    // animateScrollBy({ x: 0, y: window.innerHeight, duration: 2000, useNativeScroll: false });
   })
 })
