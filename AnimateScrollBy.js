3263aec8aab3f067c1fda24a0dfaad29141bf956
Dan Johnson <danjohnsonnj@gmail.com>
2020-12-12 11:45:10 -0500

    JSDocs

--- AnimateScrollBy.js
+++ AnimateScrollBy.js
@@ -1,6 +1,22 @@
 const animateScrollTo = require('./AnimateScrollTo');
 const getOptions = require('./GetOptions');
 
+/**
+ * Scroll the target container by a specific x and y value.
+ *
+ * Two method signature types
+ * @param {number} x - The horizontal value to scroll by.
+ * @param {number} y - The vertical value to scroll by.
+ *
+ * Or
+ * @param {args.x<number>} x - The horizontal value to scroll by.
+ * @param {args.y<number>} y - The vertical value to scroll by.
+ * @param {args.target<HTMLElement>} target - The scrolling element.
+ * @param {args.duration<number>} duration - The duration of the animation in milliseconds.
+ * @param {args.useNativeScroll<boolean>} useNativeScroll - Use native scrollTo if smooth scrolling is supported.
+
+ * @return {undefined}
+ */
 function AnimateScrollBy(...args) {
   const { x, y, target, duration, useNativeScroll } = getOptions(...args);
 
--- AnimateScrollTo.js
+++ AnimateScrollTo.js
@@ -1,6 +1,22 @@
 const { easeInOutCubic } = require('js-easing-functions');
 const getOptions = require('./GetOptions');
 
+/**
+ * Scroll the target container to a specific x and y value.
+ *
+ * Two method signature types
+ * @param {number} x - The horizontal value to scroll to.
+ * @param {number} y - The vertical value to scroll to.
+ *
+ * Or
+ * @param {args.x<number>} x - The horizontal value to scroll to.
+ * @param {args.y<number>} y - The vertical value to scroll to.
+ * @param {args.target<HTMLElement>} target - The scrolling element.
+ * @param {args.duration<number>} duration - The duration of the animation in milliseconds.
+ * @param {args.useNativeScroll<boolean>} useNativeScroll - Use native scrollTo if smooth scrolling is supported.
+ *
+ * @return {undefined}
+ */
 function AnimateScrollTo(...args) {
   const { x, y, target, duration, useNativeScroll } = getOptions(...args);
 
