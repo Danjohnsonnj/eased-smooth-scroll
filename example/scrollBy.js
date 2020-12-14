01e50cb1fe4ad97c9e39002cf76479bd0e3aad43
Dan Johnson <danjohnsonnj@gmail.com>
2020-12-13 21:50:37 -0500

    Convert to return a Promise

--- example/scrollBy.js
+++ example/scrollBy.js
@@ -2,7 +2,13 @@ const { animateScrollBy } = require('../index.js');
 
 document.querySelectorAll('section').forEach((section) => {
   section.addEventListener('click', () => {
-    // animateScrollBy(0, window.innerHeight);
-    animateScrollBy({ x: 0, y: window.innerHeight, duration: 2000, useNativeScroll: false });
-  })
+    // animateScrollBy(0, window.innerHeight)
+    //   .then(coords => console.log(coords));
+
+    animateScrollBy({
+      x: 0,
+      y: window.innerHeight,
+      duration: 500,
+      useNativeScroll: true
+    }).then(coords => console.log(coords));  })
 })
