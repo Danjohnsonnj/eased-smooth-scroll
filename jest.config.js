1d72fa0066086eb9e0bfe276bf6fe3f8e345bb4b
Dan Johnson <danjohnsonnj@gmail.com>
2020-12-31 15:47:04 -0500

    let's try dotenv w/ Travis + jest-electron-runner

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   runner: '@jest-runner/electron',
   testEnvironment: '@jest-runner/electron/environment',
   clearMocks: true,
-  verbose: true
+  verbose: true,
+  setupFiles: ['dotenv/config']
 }
